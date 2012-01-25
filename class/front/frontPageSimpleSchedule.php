f12bcc2022ab8570e9b238b97fb48b10f1d6cc36
sdk_john <sdk_john@simplexi.com>
2012-01-25 10:18:51 +0800

    * display css,image,background for test.

--- class/front/frontPageSimpleSchedule.php
+++ class/front/frontPageSimpleSchedule.php
@@ -1,9 +1,34 @@
 <?php
+require_once('builder/builderInterface.php');
 
-class frontPageSimpleSchedule
+class frontPageSimpleSchedule extends Controller_Front
 {
-    protected function run()
+    private $_sHtmlPrefix;
+    protected function run($aArgs)
     {
+        $this->_sHtmlPrefix = "sdk_simpleschedule_";
+        $sInitScript = usbuilder()->init($this->Request->getAppID(), $aArgs);
+        $this->writeJs($sInitScript);
 
+        $sHtml = '';
+        $sHtml .= "<div id='{$this->_sHtmlPrefix}holder'>";
+        $sHtml .= "<div id='{$this->_sHtmlPrefix}container'>";
+        $sHtml .= " <h3 class='{$this->_sHtmlPrefix}title'><strong>{$sModuleTitle}</strong></h3>";
+        $sHtml .= "    <div id='{$this->_sHtmlPrefix}calendar'></div>";
+        $sHtml .= "    <div id='{$this->_sHtmlPrefix}expand'>";
+        $sHtml .= "    <div class='pg_scheduleradv_overlay3'>";
+        $sHtml .= "    <div class='pg_scheduleradv_overlay4'>";
+        $sHtml .= "    <div class='pg_scheduleradv_loader'></div>";
+        $sHtml .= "    </div>";
+        $sHtml .= "    </div>";
+        $sHtml .= "    <div class='{$this->_sHtmlPrefix}output'></div>";
+        $sHtml .= "    </div>";
+        $sHtml .= "    </div>";
+        $sHtml .= "    </div>";
+        $sHtml .= "    <div class='{$this->_sHtmlPrefix}overlaycontainer'>";
+        $sHtml .= "    </div>";
+        $sHtml .= "    <input type='text' value='{$sServerUrl}' name='sServerUrl' id='sServerUrl'>";
+        $this->importCss(__CLASS__);
+        $this->assign('simpleschedule',$sHtml);
     }
 }
